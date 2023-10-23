12682d8d22b23b8fb4f60b213c6fb5d508368a42
Anna Krystalli <annakrystalli@googlemail.com>
2023-10-23 10:52:16 +0300

    add app ui, server & run_app

--- R/app_server.R
+++ R/app_server.R
@@ -5,5 +5,20 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+    options(shiny.maxRequestSize=100*1024^2)
   # Your application server logic
+    dataset_1 <- mod_access_data_server("access_data_1")
+    dataset_2 <- mod_access_data_server("access_data_2")
+
+    shared_tables <- reactive({
+      req(dataset_1)
+      req(dataset_2)
+      intersect(names(dataset_1()),
+                names(dataset_2()))
+    })
+    output$shared_files <- renderText(shared_tables())
+
+    selected_tables <- mod_select_tbls_server("select_tbls", shared_tables)
+    mod_read_tbls_server("read_tbls", selected_tables, dataset_1, dataset_2)
+
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,12 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      h1("MATCHA")
+      h1("MATCHA"),
+      mod_access_data_ui("access_data_1"),
+      mod_access_data_ui("access_data_2"),
+      textOutput("shared_files"),
+      mod_select_tbls_ui("select_tbls"),
+      mod_read_tbls_ui("read_tbls")
     )
   )
 }
