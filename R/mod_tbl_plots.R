71d35c8ac20050c117d8b37b31e4334d76145f1a
Anna Krystalli <annakrystalli@googlemail.com>
2023-12-06 11:10:54 +0200

    add modules for adding deleting variable plots per table.

---
+++ R/mod_tbl_plots.R
@@ -0,0 +1,57 @@
+#' tbl_plots UI Function
+#'
+#' @description Module for adding, removing and configuring plots for variables
+#' in a table.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_tbl_plots_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    actionButton(ns("add_plot"), "Add plot", icon = icon("plus"), width = '100%',
+                 class = "btn-success")
+  )
+}
+
+#' tbl_plots Server Functions
+#'
+#' @param tbl a list containing two elements:
+#' - `previous`: a tibble of previous data
+#' - `current`: a tibble of current data
+#' to compare.
+#' @param tbl_name Character string. The table name.
+#' @param check a list of the results of checks output from [check_tbls()].
+#' @noRd
+mod_tbl_plots_server <- function(id, tbl, tbl_name, check){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+    tbl <- validate_tbl(tbl, check)
+
+    comb_tbl <- combine_tbls(
+      current_tbl = tbl$current,
+      previous_tbl = tbl$previous
+      )
+    observeEvent(input$add_plot, {
+      insertUI(
+        selector = glue::glue("#{ns('add_plot')}"), where = "afterEnd",
+        ui = mod_var_plot_modal_ui(
+          ns(paste0("var_plot_modal_", input$add_plot))
+        )
+      )
+      mod_var_plot_modal_server(
+        paste0("var_plot_modal_", input$add_plot),
+        comb_tbl
+      )
+    })
+
+  })
+}
+
+## To be copied in the UI
+# mod_tbl_plots_ui("tbl_plots_1")
+
+## To be copied in the server
+# mod_tbl_plots_server("tbl_plots_1")
---
+++ R/mod_var_plot_modal.R
@@ -0,0 +1,90 @@
+#' var_plot_modal UI Function
+#'
+#' @description Module for launching modal to configure new plot.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS tagList
+mod_var_plot_modal_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns("plot_ui"))
+  )
+}
+
+#' var_plot_modal Server Functions
+#'
+#' @noRd
+mod_var_plot_modal_server <- function(id, comb_tbl) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+    print(paste("plot_modal_server ns", ns(NULL)))
+    print(paste("plot_modal_server plot_ui ns", ns("plot_ui")))
+
+    output$select_plot_ui <- renderUI({
+      req(input$select_var)
+      if (!is.null(input$select_var)) {
+        selectInput(ns("select_plot"),
+          "Select plot type",
+          choices = var_plot_types(input$select_var, comb_tbl),
+          multiple = FALSE,
+          selectize = TRUE
+        )
+      }
+    })
+    output$plot_ui <- renderUI({
+      req(input$select_plot)
+      if (!is.null(input$select_plot)) {
+        plot_ui_mod <- get(paste("mod", input$select_plot, "ui", sep = "_"))
+        plot_ui_mod(ns("plot"), var = input$select_var)
+      }
+    }) %>%
+      bindEvent(input$ok)
+
+    plotModal <- function(failed = FALSE) {
+      modalDialog(
+        selectInput(ns("select_var"),
+          paste("Select", "variable", "to compare"),
+          choices = names(comb_tbl),
+          multiple = FALSE,
+          selected = NULL,
+          selectize = TRUE
+        ),
+        uiOutput(ns("select_plot_ui")),
+        footer = tagList(
+          modalButton("Cancel"),
+          actionButton(ns("ok"), "OK")
+        )
+      )
+    }
+
+    observe({
+      showModal(plotModal())
+    })
+
+    observeEvent(input$ok, {
+      req(input$select_plot)
+      if (!is.null(input$select_plot)) {
+        plot_server_mod <- get(paste("mod", input$select_plot, "server", sep = "_"))
+        plot_server_mod("plot", comb_tbl, input$select_var)
+      }
+      removeModal()
+    })
+
+    reactive({
+      list(
+        var = input$select_var,
+        plot = input$select_plot
+      )
+    }) %>%
+      bindEvent(input$ok)
+  })
+}
+
+## To be copied in the UI
+# mod_var_plot_modal_ui("var_plot_modal_1")
+
+## To be copied in the server
+# mod_var_plot_modal_server("var_plot_modal_1")
