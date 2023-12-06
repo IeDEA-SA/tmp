490941bdb2b41d85f3feee153a60dff5736c2c64
Anna Krystalli <annakrystalli@googlemail.com>
2023-12-06 10:55:11 +0200

    reorganise check display

--- R/mod_display_check.R
+++ R/mod_display_check.R
@@ -8,24 +8,32 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-#' @importFrom bslib card card_header card_body
-mod_display_check_ui <- function(id){
+#' @importFrom bslib card card_header card_body accordion accordion_panel
+mod_display_check_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    h3(textOutput(ns("tbl_name"))),
-    h4(textOutput(ns("valid"))),
     card(
       card_header("Valid shared variables"),
       card_body(strong(textOutput(ns("valid_cols"))))
+    ),
+    accordion(
+      accordion_panel(
+        title = "Variable names",
+        icon = icon("font"),
+        textOutput(ns("names_msg"))
       ),
-    h4("Check tbl variable names"),
-    textOutput(ns("names_msg")),
-    hr(),
-    h4("Check tbl variable data types"),
-    textOutput(ns("coltypes_msg")),
-    hr(),
-    h4("tbl Structure"),
-    verbatimTextOutput(ns("tbl_summary"))
+      accordion_panel(
+        title = "Variable data types",
+        icon = icon("table"),
+        textOutput(ns("coltypes_msg"))
+      ),
+      accordion_panel(
+        "tbl Structure",
+        icon = icon("magnifying-glass-chart"),
+        verbatimTextOutput(ns("tbl_summary"))
+      ),
+      open = FALSE
+    )
   )
 }
 
@@ -38,24 +46,21 @@ mod_display_check_ui <- function(id){
 #' @param check a list of the results of checks output from [check_tbls()].
 #'
 #' @noRd
-mod_display_check_server <- function(id, tbl, tbl_name, check){
-  moduleServer( id, function(input, output, session){
-    print(tbl_name)
-    print(tbl)
-    print(check)
+mod_display_check_server <- function(id, tbl, tbl_name, check) {
+  moduleServer(id, function(input, output, session) {
     ns <- session$ns
-    output$tbl_name <- renderText(tbl_name)
     output$valid_cols <- renderText({
       glue::glue_collapse(
         check$check_coltypes$valid_cols,
-        sep = ", ")
-      })
+        sep = ", "
+      )
+    })
     output$tbl_summary <- renderPrint(str(tbl))
     output$names_msg <- renderText(check$check_names$msg)
     output$coltypes_msg <- renderText(check$check_coltypes$msg)
     output$valid <- renderText({
       if (check$valid) "Tables are valid" else "Tables are invalid"
     })
+    check_valid_cols <- check$check_coltypes$valid_cols
   })
 }
-
