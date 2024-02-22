486c8563c7fc6f3570c897891441b315351fc8ba
Anna Krystalli <annakrystalli@googlemail.com>
2024-02-22 18:09:37 +0200

    Draft implementation of NA coercion notification. Related to #42

--- R/apply_schema.R
+++ R/apply_schema.R
@@ -12,20 +12,46 @@ apply_schema <- function(tbl, schema_config) {
   for (var_name in names(schema_config)) {
     var_type <- schema_config[[var_name]]()$var_type
     unknown <- schema_config[[var_name]]()$unknown
+    var_type_tbl_previous <- class(tbl$previous[[var_name]])
+    var_type_tbl_current <- class(tbl$current[[var_name]])
+    coerce_both <- !is.null(var_type) && var_type_tbl_previous != var_type
+    coerce_current <- isFALSE(coerce_both) && var_type_tbl_current != var_type_tbl_previous
 
-    if (!is.null(var_type)) {
+    previous_NAs <- sum(is.na(tbl$previous[[var_name]]))
+    current_NAs <- sum(is.na(tbl$current[[var_name]]))
+
+    if (coerce_both) {
       coerce_fn <- get(sprintf("as.%s", var_type))
+      pre_NAs <- sum(is.na(tbl$previous[[var_name]]))
       tbl$previous[[var_name]] <- coerce_fn(tbl$previous[[var_name]])
       tbl$current[[var_name]] <- coerce_fn(tbl$current[[var_name]])
-    } else {
-      coerce_fn <- identity
     }
+    if (coerce_current) {
+      coerce_fn <- get(sprintf("as.%s", var_type_tbl_previous))
+      tbl$current[[var_name]] <- coerce_fn(tbl$current[[var_name]])
+    }
+
+    if (any(coerce_both, coerce_current)) {
+      purrr::map2(
+        .x = c("previous", "current"),
+        .y = c(previous_NAs, current_NAs),
+        .f = function(x, y) {
+          if (sum(is.na(tbl[[x]][[var_name]])) > y) {
+            showNotification(
+              glue::glue("NAs introduced by coercion to column {var_name} in {x} data table"),
+              type = "warning"
+            )
+          }
+        }
+      )
+    }
+
     if (is.null(unknown) || unknown == "") {
     } else {
-      make_NA <- tbl$previous[[var_name]] == coerce_fn(unknown)
+      make_NA <- as.character(tbl$previous[[var_name]]) == unknown
       tbl$previous[[var_name]][make_NA] <- NA
 
-      make_NA <- tbl$current[[var_name]] == coerce_fn(unknown)
+      make_NA <- as.character(tbl$current[[var_name]]) == unknown
       tbl$current[[var_name]][make_NA] <- NA
     }
   }
