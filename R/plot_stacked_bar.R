e6c880d29d79afc8c03de0735db5a790a668074d
Anna Krystalli <annakrystalli@googlemail.com>
2024-04-25 15:42:21 +0300

    add dependencies

--- R/plot_stacked_bar.R
+++ R/plot_stacked_bar.R
@@ -37,5 +37,5 @@ plot_stacked_bar <- function(tbl, x,
 # source: https://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
 gg_color_hue <- function(n) {
   hues <- seq(15, 375, length = n + 1)
-  hcl(h = hues, l = 65, c = 100)[1:n]
+  grDevices::hcl(h = hues, l = 65, c = 100)[seq(n)]
 }
