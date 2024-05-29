57af1f2431210902ba61904ddc1eb8d5111323a2
Anna Krystalli <annakrystalli@googlemail.com>
2024-05-29 17:15:42 +0300

    do not add pk column to pk table subset by default

--- tests/testthat/test-pk_tbl-utils.R
+++ tests/testthat/test-pk_tbl-utils.R
@@ -1,13 +1,16 @@
 test_that("subset_pk_tbl_cols works", {
   tblBAS <- load_comb_tbl("tblBAS")
-  pk_tbl <- subset_pk_tbl_cols(tblBAS)
+  pk_tbl <- subset_pk_tbl_cols(tblBAS, add_pk_col = TRUE)
   expect_false(any(is.na(pk_tbl$pk)))
   expect_snapshot(pk_tbl)
+
+  pk_tbl_nopk <- subset_pk_tbl_cols(tblBAS)
+  expect_snapshot(pk_tbl_nopk)
 })
 
 test_that("join_pk works", {
   tblBAS <- load_comb_tbl("tblBAS")
-  pk_tbl <- subset_pk_tbl_cols(tblBAS)
+  pk_tbl <- subset_pk_tbl_cols(tblBAS, add_pk_col = TRUE)
 
   tbl <- load_comb_tbl("tblART")
   rm_ids <- tbl$patient %>%
