53d7a13880d25b1c208cefe64c171432dede903b
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-01 16:41:22 +0300

    Add tests for all valid cols

--- tests/testthat/test-check_tbls.R
+++ tests/testthat/test-check_tbls.R
@@ -16,3 +16,21 @@ test_that("check tables functions work when invalid var present", {
   expect_snapshot(colytpes_check)
 })
 
+
+test_that("check tables functions work when all valid vars present", {
+
+  x_valid <- list(
+    previous = read_file(system.file("test-data/csv/01_previous/tblBAS.csv", package = "MATCHA")),
+    current = read_file(system.file("test-data/csv/02_current/tblBAS.csv", package = "MATCHA"))
+  )
+  names_checks_all_valid <- check_tbl_names(x_valid)
+  expect_snapshot(names_checks_all_valid)
+
+  x_valid <- process_tbl(x_valid, clean_names = names_checks_all_valid$clean_names,
+                   select_vars = names_checks_all_valid$valid_cols)
+
+  expect_snapshot(str(x_valid))
+
+  colytpes_check_all_valid <- check_tbl_coltypes(x_valid)
+  expect_snapshot(colytpes_check_all_valid)
+})
