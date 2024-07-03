1741e5f0709360e80fdfb74f2e66180c3fb993eb
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-03 17:11:55 +0300

    Add na.rm argument to plot_stacked_bar

--- R/plot_stacked_bar.R
+++ R/plot_stacked_bar.R
@@ -3,20 +3,33 @@
 #'
 #' @param tbl Combined table of previous and current data. Output of [combine_tbls()].
 #' @param x Character string. Name of variable in table to plot.
-#' @param position Character string.  Bar position. Either "fill" (percentages) or "stack" (counts).
-#' @param n Integer. Number of most common categories to display. All other categories recoded to "Other".
+#' @param position Character string.  Bar position. Either "fill" (percentages)
+#' or "stack" (counts).
+#' @param n Integer. Number of most common categories to display. All other
+#' categories recoded to "Other".
+#' @param na.rm Logical. Remove NA values from the table.
+#'
 #' @importFrom ggplot2 ggplot aes geom_bar scale_fill_manual ylab scale_y_continuous
+#' @importFrom forcats fct_infreq fct_lump_n
+#' @importFrom scales percent
+#' @importFrom dplyr filter
+#' @importFrom rlang arg_match
 #' @return Stacked Bar plot ggplot plot.
 #' @export
 plot_stacked_bar <- function(tbl, x,
                              position = c(
                                "fill", "stack"
                              ),
-                             n = 8L) {
-  position <- rlang::arg_match(position)
+                             n = 8L,
+                             na.rm = TRUE) {
+  position <- arg_match(position)
   palette <- c(gg_color_hue(n), "grey")
-  tbl[[x]] <- forcats::fct_infreq(tbl[[x]]) %>%
-    forcats::fct_lump_n(n)
+  if (na.rm) {
+    tbl <- filter(tbl, !is.na(.data[[x]]))
+  }
+
+  tbl[[x]] <- fct_infreq(tbl[[x]]) %>%
+    fct_lump_n(n)
 
   p <- tbl %>%
     ggplot(aes(x = .data[["tbl"]], fill = .data[[x]])) +
@@ -28,7 +41,7 @@ plot_stacked_bar <- function(tbl, x,
 
   if (position == "fill") {
     p <- p +
-      scale_y_continuous(labels = scales::percent) +
+      scale_y_continuous(labels = percent) +
       ylab("Percentage")
   }
   p
