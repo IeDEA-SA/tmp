418f707091994febc99d140430bf86fe60482128
Anna Krystalli <annakrystalli@googlemail.com>
2024-07-26 17:48:22 +0300

    Add pdf option. Resolves #100

--- R/mod_download.R
+++ R/mod_download.R
@@ -7,10 +7,16 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
+#' @importFrom shinyWidgets awesomeRadio
 mod_download_ui <- function(id) {
   ns <- NS(id)
   tagList(
     downloadButton(ns("report"), "Generate report", class = "btn-secondary"),
+    awesomeRadio(ns("format"), "Select report format:",
+                 choices = c("html", "pdf"),
+                 selected = "html",
+                 inline = TRUE,
+                 checkbox = TRUE)
   )
 }
 
@@ -23,7 +29,8 @@ mod_download_server <- function(id) {
 
     output$report <- downloadHandler(
       filename = function() {
-        paste0("report_", Sys.Date(), ".html")
+        glue::glue("report_{Sys.Date()}.{input$format}")
+        #paste0("report_", Sys.Date(), ".html")
       },
       content = function(file) {
         w <- add_waiter(msg = "Generating report...")
@@ -32,10 +39,14 @@ mod_download_server <- function(id) {
         report_path <- system.file("report.Rmd", package = "MATCHA")
         file.copy(report_path, tempReport, overwrite = TRUE)
 
+        format <- switch(input$format,
+                         html = "html_document",
+                         pdf = "pdf_document")
 
         rmarkdown::render(
           input = tempReport,
           output_file = file,
+          output_format = format,
           params = list(
             plots = session$userData$plots
           )
