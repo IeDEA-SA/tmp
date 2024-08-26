c76f8eedee675825375c72c9161b71a680fea982
Anna Krystalli <annakrystalli@googlemail.com>
2024-08-26 11:05:19 +0300

    Add note to run_app docs re passing pk_tbl_name and pk_col via ...

--- R/run_app.R
+++ R/run_app.R
@@ -1,12 +1,19 @@
 #' Run the Shiny Application
 #'
-#' @param ... arguments to pass to golem_opts.
-#' See `?golem::get_golem_options` for more details.
+#' @param ... arguments to pass to golem_opts. E.g. use these to override the
+#' primary key table and column at runtime through arguments `pk_tbl_name`
+#' and `pk_col` respectively. See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
 #'
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
+#' @examples
+#' \dontrun{
+#' run_app()
+#' # Override the default primary key source settings
+#' run_app(pk_tbl_name = "tblKEYS", pk_col = "id")
+#' }
 run_app <- function(onStart = NULL,
                     options = list(),
                     enableBookmarking = NULL,
